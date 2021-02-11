1d2cf9eda7efbbf6b3ccf620ce41ca7c4b90b485
StrekanovAlexander <strekanov.alexander@gmail.com>
2021-02-11 17:55:59 +0200

    added: Eloquent ORM

--- core/app.php
+++ core/app.php
@@ -1,14 +1,25 @@
 <?php
 
 require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../core/db.php');
 
 $app = new \Slim\App([
     'settings' => [
        'displayErrorDetails' => true,
+       'db' => $db,
     ]
 ]);
 
 $c = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($c['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$c['db'] = function() use ($capsule) {
+  return $capsule;
+};
 
 $c['view'] = function($c) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../core/views', [
---
+++ core/db.php
@@ -0,0 +1,12 @@
+<?php
+
+$db = [
+  'driver' => 'mysql',
+  'host' => 'localhost',
+  'database' => 'elg_db',
+  'username' => 'root',
+  'password' => 'root',
+  'charset' => 'utf8',
+  'collation' => 'utf8_general_ci',
+  'prefix' => 'elg_',
+];
