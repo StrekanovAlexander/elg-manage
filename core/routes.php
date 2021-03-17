add7a7f1b8012afdc408dde964058e1286013e3f
StrekanovAlexander <strekanov.alexander@gmail.com>
2021-03-17 09:31:42 +0200

    rebuilded: home route

--- core/routes.php
+++ core/routes.php
@@ -1,7 +1,8 @@
 <?php
 
 $app->group('', function() {
-    $this->get('/', 'HomeController:index')->setName('home.index');
+    // $this->get('/', 'HomeController:index')->setName('home.index');
+    $this->get('/', 'RuleController:getRules')->setName('home.index');
 
     $this->get('/base-rates/create', 'BaseRateController:create')->setName('base-rate.create');
     $this->post('/base-rates/create', 'BaseRateController:store');
