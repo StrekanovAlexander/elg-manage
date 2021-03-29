84a15061a56d457fb412b6b4d1a54e8ae3d6ade5
StrekanovAlexander <strekanov.alexander@gmail.com>
2021-03-29 15:33:05 +0300

    rebuilded: with cors for getting json data

--- core/Controllers/RateController.php
+++ core/Controllers/RateController.php
@@ -40,7 +40,11 @@ class RateController extends Controller
         } 
 
         $res->getBody()->write(json_encode($json, JSON_UNESCAPED_UNICODE));
-        return  $res->withHeader('Content-type', 'application/json; charset=utf-8');
+        // return  $res->withHeader('Content-type', 'application/json; charset=utf-8');
+        return  $res->withHeader('Content-type', 'application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
     }
 
